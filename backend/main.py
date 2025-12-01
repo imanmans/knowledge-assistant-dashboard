933d873499242810ee54802b5e1dec1beaab3329
Iman Mansour <iman@45db56ce-09aa-4aa2-ad6d-a22dd102ed6c.fritz.box>
2025-12-01 23:04:38 +0100

    Backend/ Enhance upload files API - add background task

--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,24 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, BackgroundTasks
 from typing import List
 from processing import get_file_content, get_chunks, get_embeddings, store_embeddings, list_uploaded_docs
 from chat_helper import generate_answer
 
 app = FastAPI()
 
+def process_file(file: UploadFile):
+    # Extract text content
+    content = get_file_content(file)
+    collection_name = f"{file.filename.replace(' ', '_').replace('.pdf', '').replace('.txt', '')}"
+    # Process text: chunking, embedding, storing
+    chunks = get_chunks(content)
+    embeddings = get_embeddings(chunks)
+    store_embeddings(chunks, embeddings, collection_name)
+
 @app.post("/api/upload/")
-async def upload_files(files: List[UploadFile] = File(..., description="Multiple files as UploadFile")):
+async def upload_files(files: List[UploadFile] = File(...), background_tasks: BackgroundTasks = None):
     for file in files:
-        # Extract text content
-        content = get_file_content(file)
-        collection_name = f"{file.filename.replace(' ', '_').replace('.pdf', '').replace('.txt', '')}"        
-        # Process text: chunking, embedding, and storing
-        chunks = get_chunks(content)
-        embeddings = get_embeddings(chunks)
-        store_embeddings(chunks, embeddings, collection_name)
-    return {"message": "Files processed successfully."}
+        background_tasks.add_task(process_file, file)
+    return {"message": "Files uploaded successfully. Processing in background."}
 
 @app.get("/api/docs/")
 def list_collections():
